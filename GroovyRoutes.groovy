fb1a80d1d7ec94925e42a7e6a4e9ba661d7477a7
Chris Sterling <csterling@pivotal.io>
2019-01-15 10:13:26 -0800

    Update GroovyRoutes.groovy

--- GroovyRoutes.groovy
+++ GroovyRoutes.groovy
@@ -6,9 +6,9 @@ class GroovyRoutes {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("resource", r -> r.path("/google"))
-                .uri("http://www.google.com"))
-                .build();
+            .route("resource", { r -> r.path("/google") })
+            .uri("http://www.google.com"))
+            .build();
     }
 
 }
