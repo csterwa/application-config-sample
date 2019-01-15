744d8f01ac0190ac607f9659f4155f17f9f8df4c
Chris Sterling <csterling@pivotal.io>
2019-01-15 10:10:31 -0800

    Added missing paren

--- GroovyRoutes.groovy
+++ GroovyRoutes.groovy
@@ -6,7 +6,7 @@ class GroovyRoutes {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("resource", r -> r.path("/google")
+                .route("resource", r -> r.path("/google"))
                 .uri("http://www.google.com"))
                 .build();
     }
